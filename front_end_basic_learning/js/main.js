c3dcf4477d8e17eaf73262db2b8c95113b0d13a6
Yufan Tan <yufan_tan0406@163.com>
2026-07-19 16:22:03 +0800

    main文件中的文件导入有问题，进行了修改

--- front_end_basic_learning/js/main.js
+++ front_end_basic_learning/js/main.js
@@ -1,8 +1,8 @@
 // 作为入口文件，统一调用各个模块的初始化函数
 
-import { initCardsAnim } from "./cards";
-import { initNavAnim } from "./nav";
-import { initScoreAnim } from "./score";
+import { initCardsAnim } from "./cards.js";
+import { initNavAnim } from "./nav.js";
+import { initScoreAnim } from "./score.js";
 
 initCardsAnim();
 initNavAnim();
