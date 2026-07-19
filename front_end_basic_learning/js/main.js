34c8e087cdc931721042b734e9d50aea71bd885c
Yufan Tan <yufan_tan0406@163.com>
2026-07-19 16:29:22 +0800

    修改了main.js中对于导航模块的错误调用

--- front_end_basic_learning/js/main.js
+++ front_end_basic_learning/js/main.js
@@ -1,9 +1,9 @@
 // 作为入口文件，统一调用各个模块的初始化函数
 
 import { initCardsAnim } from "./cards.js";
-import { initNavAnim } from "./nav.js";
+import { initNav } from "./nav.js";
 import { initScoreAnim } from "./score.js";
 
 initCardsAnim();
-initNavAnim();
+initNav();
 initScoreAnim();
