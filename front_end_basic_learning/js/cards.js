b8f99fd7cd3728f95fcde93842e60618443f742b
Yufan Tan <yufan_tan0406@163.com>
2026-07-19 16:26:53 +0800

    修正了cards.js中的bug，修改了函数调用代码

--- front_end_basic_learning/js/cards.js
+++ front_end_basic_learning/js/cards.js
@@ -1,10 +1,10 @@
 import {animate, stagger} from "https://cdn.jsdelivr.net/npm/animejs@4/+esm";
 
 export function initCardsAnim() {
-  anime.animate(".card", {
+  animate(".card", {
     opacity: [0, 1],
     translateY: [24, 0],
-    delay: anime.stagger(120),
+    delay: stagger(120),
     duration: 700,
     ease: "outBack",
   });
